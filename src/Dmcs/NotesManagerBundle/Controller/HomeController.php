c18b8e3f7bb11ba4a9007a067973bd0ca7390200
mic4ael <crossner90@gmail.com>
2014-11-04 16:14:36 +0100

    Basic logging to the application

---
+++ src/Dmcs/NotesManagerBundle/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dmcs\NotesManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class HomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('DmcsNotesManagerBundle:Home:index.html.twig');
+    }
+}
---
+++ src/Dmcs/NotesManagerBundle/Tests/Controller/HomeControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dmcs\NotesManagerBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+}
